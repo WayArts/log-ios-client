17cd889e1210dee6b6b55d8d9975da51b86b990d
Sasha Human <sahlet.official@gmail.com>
2023-04-16 14:59:54 -0700

    Update

--- src/bot/index.js
+++ src/bot/index.js
@@ -1,20 +1,36 @@
-const TelegramBot = require("telegraf");
+const { Telegraf, Markup } = require("telegraf");
 require("dotenv").config();
 
 const TOKEN = process.env.TOKEN;
 
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(TOKEN, {polling: true});
+const bot = new Telegraf(TOKEN);
 
-// Matches "/echo [whatever]"
+const viewDataButtonText = 'View data';
+const helpButtonText = 'Help';
+const watchDataTypesButtonText = 'Watch data types';
 
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on('message', (msg) => {
-    console.log(msg.text);
 
-    const chatId = msg.chat.id;
-
-    // send a message to the chat acknowledging receipt of their message
-    bot.sendMessage(chatId, 'Received your message');
+bot.start((ctx) => {
+    ctx.reply('Watch menu', Markup
+      .keyboard([
+        [viewDataButtonText, helpButtonText], // Row1 with 2 buttons
+        [watchDataTypesButtonText], // Row2 with 2 buttons
+      ])
+      .oneTime()
+      .resize()
+    );
 });
+
+bot.hears(viewDataButtonText, ctx => {
+    ctx.reply('on ' + viewDataButtonText);
+});
+
+bot.hears(helpButtonText, ctx => {
+    ctx.reply('on ' + helpButtonText);
+});
+
+bot.hears(watchDataTypesButtonText, ctx => {
+    ctx.reply('on ' + watchDataTypesButtonText);
+});
+
+bot.launch();
